52d9636b459e8abd14d7019f4d06160eeea3984b
Christian Oestreich <acetrike@yahoo.com>
2014-03-27 15:40:52 -0500

    removing cache plugin

--- grails-app/controllers/com/bsb/DataController.groovy
+++ grails-app/controllers/com/bsb/DataController.groovy
@@ -1,16 +1,13 @@
 package com.bsb
 
 import grails.converters.JSON
-import grails.plugin.cache.Cacheable
 
 class DataController {
 
-//    @Cacheable(value = "serviceTypesCache")
     def serviceTypes() {
         render ServiceType.values() as JSON
     }
 
-//    @Cacheable(value = "environmentTypesCache")
     def environmentTypes() {
         render EnvironmentType.values() as JSON
     }
